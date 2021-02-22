2807effaa5776e24118cd3994a53d00742d34a78
Darren Darlington <darrend42@outlook.com>
2021-02-22 10:28:51 -0600

    finished setting up alert

--- Programming Jokes/ContentView.swift
+++ Programming Jokes/ContentView.swift
@@ -8,28 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert = false
     @State private var jokes = [Joke]()
+    
     var body: some View {
         NavigationView {
             List(jokes) { joke in
                 NavigationLink(
                     destination: Text(joke.punchline)
-                    .padding(),
-                label: {
-                    Text(joke.setup)
-                })
+                        .padding(),
+                    label: {
+                        Text(joke.setup)
+                    })
             }
             .navigationTitle("Programming Jokes")
         }
         .onAppear(perform: {
             getJokes()
         })
+        .alert(isPresented: $showingAlert, content: {
+            Alert(title: Text("Loading Error"),
+            message: Text("There was a problem loading the data"),
+            dismissButton: .default(Text("Ok")))
+        })
     }
     
     func getJokes() {
-       let apiKey = "?rapidapi-key=32a3795ffdmshbeee571acffa848p1c5fecjsn7024003cbda5"
-    }
-}
+           let apiKey = "?rapidapi-key=32a3795ffdmshbeee571acffa848p1c5fecjsn7024003cbda5"
+           let query = "https://dad-jokes.p.rapidapi.com/joke/type/programming\(apiKey)"
+           if let url = URL(string: query) {
+               if let data = try? Data(contentsOf: url) {
+                   let json = try! JSON(data: data)
+                   if json["success"] == true {
+                       let contents = json["body"].arrayValue
+                       for item in contents {
+                           let setup = item["setup"].stringValue
+                           let punchline = item["punchline"].stringValue
+                           let joke = Joke(setup: setup, punchline: punchline)
+                           jokes.append(joke)
+                       }
+                    return
+                   }
+               }
+           }
+        showingAlert = true
+       }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
@@ -41,4 +64,5 @@ struct Joke: Identifiable {
     let id = UUID()
     var setup: String
     var punchline: String
+}
 }
